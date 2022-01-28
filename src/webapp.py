7e6625ea6996699b4d64e23b7b9e647d02e500d0
Brian Chen <brian.chen20@det.nsw.edu.au>
2022-01-28 17:30:36 +1100

    main() function

--- src/webapp.py
+++ src/webapp.py
@@ -11,19 +11,25 @@ import logging.config
 
 importlib.reload(sys)
 
-#logging.config.fileConfig("logging.conf")
-LOGLEVEL = os.environ.get('LOGLEVEL', 'INFO').upper()
-logging.basicConfig(level=LOGLEVEL, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-if sys.version[0] == '2':
-    from imp import reload
+# logging.config.fileConfig("logging.conf")
 
-    reload(sys)
-    sys.setdefaultencoding("utf-8")
+def main():
+    LOGLEVEL = os.environ.get('LOGLEVEL', 'INFO').upper()
+    logging.basicConfig(level=LOGLEVEL, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-debug = False
+    if sys.version[0] == '2':
+        from imp import reload
 
-if 'DEBUG' in os.environ and os.environ['DEBUG'] == "1":
-    debug = True
+        reload(sys)
+        sys.setdefaultencoding("utf-8")
 
-app.run(debug=debug, host='0.0.0.0', port=8080)
+    debug = False
+
+    if 'DEBUG' in os.environ and os.environ['DEBUG'] == "1":
+        debug = True
+
+    app.run(debug=debug, host='0.0.0.0', port=8080)
+
+
+main()
